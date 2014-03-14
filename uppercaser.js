6789ccef67dde540b2418661cf677d9ee3b3f901
Pascal <pascal@pascal.com>
2014-03-14 03:40:44 -0400

    uppercaser.js

--- uppercaser.js
+++ uppercaser.js
@@ -2,7 +2,8 @@ var http = require('http')
 var map = require('through2-map')
 
 var server = http.createServer(function (req, res) {
-	res.writeHead(200, { 'content-type': 'text/plain' })
+	if (req.method != 'POST')
+		return res.end('send me a POST\n')
 
 	req.pipe(map(function(chunk) {
 		return chunk.toString().toUpperCase()
